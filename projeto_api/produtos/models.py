e4b58ab72e9fe6cb0603032a85fcd7d56fdd0a54
Jujs2525 <juliacordeiro200.deve2024@gmail.com>
2025-04-10 12:07:40 -0300

    produtos/models.py

--- projeto_api/produtos/models.py
+++ projeto_api/produtos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Categoria (models.Model):
+    nome= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+    
+class Produto (models.Model):
+    nome= models.CharField(max_lenght=100)
+    descricao= models.TetxtField()
+    preco= models.DecimalField(max_digits= 10, decimal_places= 2)
+    categoria= models.ForeignKey(Categoria, related_name='produtos', on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.nome
 # Create your models here.
